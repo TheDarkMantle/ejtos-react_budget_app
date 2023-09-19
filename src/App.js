82383a91f5d8e9bab5caddeaf76ef11a169643f7
Shawn Rudolph <darkmantle@gmail.com>
2023-09-19 11:24:56 -0400

    pushing all changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 
 // Add code to import the other components here under
 
@@ -17,26 +21,34 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             /* Add Budget component here */
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
                         }        
 
                         {
                             /* Add ExpenseTotal component here */
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                            </div>
                         }        
                        
                         {
                             /* Add ExpenseList component here */
+                            <ExpenseList />
                         }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
+     
 
                         {
                             /* Add AllocationForm component here under */
+                            <AllocationForm />
                         }        
 
                 </div>
