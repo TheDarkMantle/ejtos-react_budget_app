71899545bbf395bcbe547765ea2d15887770d22f
Shawn Rudolph <darkmantle@gmail.com>
2023-10-13 11:25:43 -0400

    fixing budget allocation

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,16 +1,28 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+    const { budget, dispatch, expenses } = useContext(AppContext);
+    //const [newBudget, setNewBudget] = useState(budget);
+
+    const handleBudgetChange = (val) => {
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+
+        if (val < totalExpenses) {
+            alert("You cannot reduce the budget below what is already allocated");
+        } else {
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: val,
+            })
+        }
     }
     return (
         <div className='alert alert-secondary'>
             <span>Budget: ${budget}</span>
-            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+            <input type="number" step="10" value={budget} onInput={(event) =>handleBudgetChange(event.target.value)}></input>
         </div>
     );
 };
